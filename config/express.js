5ae55b7cffa18898b254696812f9a77bc7fb5648
Eric Petersen <eric@huskers.unl.edu>
2014-10-17 16:55:39 -0500

    Using some middlewares

--- config/express.js
+++ config/express.js
@@ -1,7 +1,25 @@
-var express = require('express');
+var express = require('express'),
+    morgan = require('morgan'),
+    compress = require('compression'),
+    bodyParser = require('body-parser'),
+    methodOverride = require('method-override');
 
 module.exports = function() {
     var app = express();
+    var env = process.env.NODE_ENV;
+
+    if (env === 'dev') {
+        app.use(morgan('dev'));
+    } else if (env === 'prod') {
+        app.use(compress());
+    }
+
+    app.use(bodyParser.urlencoded({
+        extended: true
+    }));
+    app.use(bodyParser.json());
+    app.use(methodOverride());
+
     require('../app/routes/index.server.routes.js')(app);
     return app;
 };
