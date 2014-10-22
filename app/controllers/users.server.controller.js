19da24687c742dddb848ecd6c46d13a752476bb7
Eric Petersen <eric@huskers.unl.edu>
2014-10-22 17:05:24 -0500

    Adding user deletion controller

--- app/controllers/users.server.controller.js
+++ app/controllers/users.server.controller.js
@@ -36,6 +36,16 @@ exports.update = function(req, res, next) {
     });
 };
 
+exports.delete = function(req, res, next) {
+    req.user.remove(function(err){
+        if (err) {
+            return next(err);
+        } else {
+            res.json(req.user);
+        }
+    });
+};
+
 // middleware to attach User to request
 exports.userById = function(req, res, next, id) {
     User.findOne(
