0a8e8b24bed9ad422ee9d27b49a6d8d30644490c
Eric Petersen <eric@huskers.unl.edu>
2014-10-22 16:37:24 -0500

    Adding user middleware and user querying controller

--- app/controllers/users.server.controller.js
+++ app/controllers/users.server.controller.js
@@ -21,3 +21,23 @@ exports.list = function(req, res, next) {
         }
     });
 };
+
+exports.read = function(req, res, next) {
+    res.json(req.user);
+};
+
+// middleware to attach User to request
+exports.userById = function(req, res, next, id) {
+    User.findOne(
+        {_id: id},
+        function(err, user) {
+            console.log(err);
+            if (err) {
+                return next(err);
+            } else {
+                req.user = user;
+                next();
+            }
+        }
+    );
+};
