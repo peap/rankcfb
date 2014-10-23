5c000c3fd3d080270e8f838ef9db0c0ad62dfe20
Eric Petersen <eric@huskers.unl.edu>
2014-10-23 12:14:05 -0500

    Setting trim options in UserSchema

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -2,11 +2,26 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
-    firstName: String,
-    lastName: String,
-    email: String,
-    username: String,
-    password: String,
+    firstName: {
+        type: String,
+        trim: true,
+    },
+    lastName: {
+        type: String,
+        trim: true,
+    },
+    email: {
+        type: String,
+        trim: true,
+    },
+    username: {
+        type: String,
+        trim: true,
+    },
+    password: {
+        type: String,
+        trim: false,
+    },
     created: {
         type: Date,
         default: Date.now,
