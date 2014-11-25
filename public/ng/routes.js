509beb10a47f91be8e469b8be1569ef30c640fea
Eric Petersen <eric@huskers.unl.edu>
2014-11-25 11:21:33 -0600

    Adding angular home route and putting ng-view below jumbotron

--- public/ng/routes.js
+++ public/ng/routes.js
@@ -3,6 +3,9 @@ angular.module('teams').config(['$routeProvider', function($routeProvider) {
         when('/teams/', {
             templateUrl: 'ng/teams/views.html',
         }).
+        when('/', {
+            templateUrl: 'ng/home/views.html',
+        }).
         otherwise({
             redirectTo: '/',
         });
