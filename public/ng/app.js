7e628b531c87719118fa51bc5336e647ebce8eec
Eric Petersen <eric@huskers.unl.edu>
2014-11-25 10:33:15 -0600

    Using hashbangs

--- public/ng/app.js
+++ public/ng/app.js
@@ -2,6 +2,10 @@ var mainAppName = 'rankcfb';
 
 var mainAppModule = angular.module(mainAppName, ['ngRoute', 'teams']);
 
+mainAppModule.config(['$locationProvider', function($locationProvider) {
+    $locationProvider.hashPrefix('!');
+}]);
+
 angular.element(document).ready(function() {
     angular.bootstrap(document, [mainAppName]);
 });
