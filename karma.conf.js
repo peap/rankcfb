374f01099d5cb88f61a2f8fd4e5b0b5b64addac1
Eric Petersen <eric@huskers.unl.edu>
2014-12-16 10:06:42 -0600

    Adding karma setup and installing angular-mocks

---
+++ karma.conf.js
@@ -0,0 +1,18 @@
+module.exports = function(config) {
+    config.set({
+        frameworks: ['jasmine'],
+        files: [
+            'public/lib/angular/angular.js',
+            'public/lib/angular-mocks/angular-mocks.js',
+            'public/lib/angular-resource/angular-resource.js',
+            'public/lib/angular-route/angular-route.js',
+            'public/ng/app.js',
+            'public/ng/*/*.js',
+            'public/ng/tests/unit/*.js',
+        ],
+        reporters: ['progress'],
+        browsers: ['PhantomJS'],
+        captureTimeout: 60000,
+        singleRun: true,
+    });
+};
