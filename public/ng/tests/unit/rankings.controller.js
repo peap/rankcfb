14888319cfc49ffbce151e75e1dc68f729c68cf5
Eric Petersen <eric@huskers.unl.edu>
2014-12-16 11:12:17 -0600

    Adding a missing inject() in the tests and loading module.js files first, since that appears to be necessary...

--- public/ng/tests/unit/rankings.controller.js
+++ public/ng/tests/unit/rankings.controller.js
@@ -25,45 +25,49 @@ describe('Testing Rankings controller', function() {
     });
 
     it('Should have a find method that uses $resource to retrieve a list of rankings',
-        inject(function($httpBackend) {
-            var sampleRanking = new Rankings({
-                name: 'ng test ranking',
-                description: 'ng test description',
-                formula: 'ngx + ngy',
-                published: true,
+        inject(function(Rankings) {
+            inject(function($httpBackend) {
+                var sampleRanking = new Rankings({
+                    name: 'ng test ranking',
+                    description: 'ng test description',
+                    formula: 'ngx + ngy',
+                    published: true,
+                });
+                var sampleRankings = [sampleRanking];
+
+                $httpBackend
+                    .expectGET('api/rankings')
+                    .respond(sampleRankings);
+
+                _scope.find();
+                $httpBackend.flush();
+
+                expect(_scope.rankings).toEqualData(sampleRankings);
             });
-            var sampleRankings = [sampleRanking];
-
-            $httpBackend
-                .expectGET('api/rankings')
-                .respond(sampleRankings);
-
-            _scope.find();
-            $httpBackend.flush();
-
-            expect(_scope.rankings).toEqualData(sampleRankings);
         })
     );
 
     it('Should have a findOne method that uses $resource to retrieve a single ranking',
-        inject(function($httpBackend, $routeParams) {
-            var sampleRanking = new Rankings({
-                name: 'ng test ranking',
-                description: 'ng test description',
-                formula: 'ngx + ngy',
-                published: true,
+        inject(function(Rankings) {
+            inject(function($httpBackend, $routeParams) {
+                var sampleRanking = new Rankings({
+                    name: 'ng test ranking',
+                    description: 'ng test description',
+                    formula: 'ngx + ngy',
+                    published: true,
+                });
+
+                $routeParams.rankingId = 'abcdef123456789012345678';
+
+                $httpBackend
+                    .expectGET(/api\/rankings\/([0-9a-fA-F]{24})$/)
+                    .respond(sampleRanking);
+
+                _scope.findOne();
+                $httpBackend.flush();
+
+                expect(_scope.ranking).toEqualData(sampleRanking);
             });
-
-            $routeParams.rankingId = 'abcdef123456789012345678';
-
-            $httpBackend
-                .expectGET(/api\/rankings\/([0-9a-fA-F]{24})$/)
-                .respond(sampleRanking);
-
-            _scope.findOne();
-            $httpBackend.flush();
-
-            expect(_scope.ranking).toEqualData(sampleRanking);
         })
     );
 
